c69c36fc7a991c2dd9caec9e1b3aba5bc6a9ce4b
Dmitry Shmelev <avikez@gmail.com>
2016-09-21 21:42:20 +0300

    Refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
-FROM node:4.4
-EXPOSE 80
-COPY server.js .
-CMD node server.js
+FROM node:argon
+
+# Create app directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package.json /usr/src/app/
+RUN npm install
+
+# Bundle app source
+COPY . /usr/src/app
+
+EXPOSE 8080
+CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-var http = require('http');
-var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
-  response.writeHead(200);
-  response.end('Hello Dima!');
-};
-var www = http.createServer(handleRequest);
-www.listen(80);
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
