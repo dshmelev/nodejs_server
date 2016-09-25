f8576d5b4c100038fb27f2d9ea362aad6b23317c
Dmitry Shmelev <avikez@gmail.com>
2016-09-25 21:11:59 +0300

    Add logs and clean

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
+require('console-stamp')(console, 'HH:MM:ss.l');
 
 const express = require('express');
+const util = require("util");
 
 // Constants
 const PORT = 8080;
@@ -8,6 +10,7 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
+  console.log('Get request: ' + req.headers.host + ' ' + req.method + ' ' + req.url )
   res.send('Hello Dima\n');
 });
 
